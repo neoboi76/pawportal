189aa0575040141b590b5c5c193523624b8c3888
neoboi76 <dattimbol@mymail.mapua.edu.ph>
2025-11-05 11:40:20 +0800

    Fix login authentication bug

--- frontend/src/app/components/footer.component/footer.component.ts
+++ frontend/src/app/components/footer.component/footer.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TokenStorageService } from '../../services/token-storage.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -8,8 +10,38 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
   currentYear = new Date().getFullYear();
+  isLoggedIn = false;
+  private userSubscription?: Subscription;
+  userName: any;
+
+  constructor(
+    private tokenStorageService: TokenStorageService
+  ) {}
+
+  checkLoginStatus(): void {
+    const token = this.tokenStorageService.getToken();
+    this.isLoggedIn = !!token;
+    
+    if (this.isLoggedIn) {
+      this.userSubscription = this.tokenStorageService.getUser().subscribe(user => {
+        if (user) {
+          this.userName = user.firstName || '';
+        }
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.checkLoginStatus();
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
 
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
